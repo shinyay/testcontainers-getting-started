e5d82f05ddfff8326a6330bd44a2ff5f4db80d36
shinyay <shinya.com@gmail.com>
2020-10-30 18:04:21 +0900

    Add PostgreSQL Container definition as Companion Object

--- src/test/kotlin/com/google/shinyay/TestcontainersGettingStartedApplicationTests.kt
+++ src/test/kotlin/com/google/shinyay/TestcontainersGettingStartedApplicationTests.kt
@@ -2,12 +2,14 @@ package com.google.shinyay
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.testcontainers.containers.PostgreSQLContainer
 
 @SpringBootTest
 class TestcontainersGettingStartedApplicationTests {
 
-	@Test
-	fun contextLoads() {
+	companion object {
+
+		val container = PostgreSQLContainer<Nothing>("postgres:12")
 	}
 
 }
