6726dc2f89068f2e7fb6bf411b8b6207c5e4d65f
shinyay <shinya.com@gmail.com>
2020-10-30 18:06:52 +0900

    Add @Container annotation to tell JUnit to notify the field

--- src/test/kotlin/com/google/shinyay/TestcontainersGettingStartedApplicationTests.kt
+++ src/test/kotlin/com/google/shinyay/TestcontainersGettingStartedApplicationTests.kt
@@ -3,12 +3,14 @@ package com.google.shinyay
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
 
 @SpringBootTest
 class TestcontainersGettingStartedApplicationTests {
 
 	companion object {
 
+		@Container
 		val container = PostgreSQLContainer<Nothing>("postgres:12")
 	}
 
