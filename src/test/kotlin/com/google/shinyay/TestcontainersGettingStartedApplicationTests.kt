22b8d408c35fd4f6070210b4c9ae10e5254335f3
shinyay <shinya.com@gmail.com>
2020-10-30 18:28:07 +0900

    Add @Testcontainers to register Junit 5 extensions

--- src/test/kotlin/com/google/shinyay/TestcontainersGettingStartedApplicationTests.kt
+++ src/test/kotlin/com/google/shinyay/TestcontainersGettingStartedApplicationTests.kt
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
+@Testcontainers
 @SpringBootTest
 class TestcontainersGettingStartedApplicationTests {
 
