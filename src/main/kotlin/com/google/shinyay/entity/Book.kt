58ccc7d5ecad77e5dbcf3ae7ba2ac8ef0e13ef71
shinyay <shinya.com@gmail.com>
2020-10-31 21:56:33 +0900

    Add GeneratedValue annotation

--- src/main/kotlin/com/google/shinyay/entity/Book.kt
+++ src/main/kotlin/com/google/shinyay/entity/Book.kt
@@ -1,9 +1,12 @@
 package com.google.shinyay.entity
 
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
 data class Book(@Id
+                @GeneratedValue(strategy = GenerationType.IDENTITY)
                 var id: Long) {
 }
