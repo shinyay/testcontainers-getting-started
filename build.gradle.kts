b723988f0dce72edd95d9d638740e22486632be9
shinyay <shinya.com@gmail.com>
2020-10-31 21:49:06 +0900

    Add No-arg compiler plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 	kotlin("jvm") version "1.3.72"
 	kotlin("plugin.spring") version "1.3.72"
 	kotlin("plugin.jpa") version "1.3.72"
+	kotlin("plugin.noarg") version "1.3.72"
 }
 
 group = "com.google.shinyay"
