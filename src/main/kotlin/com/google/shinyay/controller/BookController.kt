48c238d7ced65ae8fb7f0f06e11e9a4cddc398d5
shinyay <shinya.com@gmail.com>
2020-10-31 22:20:53 +0900

    Add find All functions

--- src/main/kotlin/com/google/shinyay/controller/BookController.kt
+++ src/main/kotlin/com/google/shinyay/controller/BookController.kt
@@ -1,7 +1,12 @@
 package com.google.shinyay.controller
 
+import com.google.shinyay.repository.BookRepository
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class BookController {
+class BookController(val repository: BookRepository) {
+
+    @GetMapping("/books")
+    fun getAddBooks() = repository.findAll()
 }
