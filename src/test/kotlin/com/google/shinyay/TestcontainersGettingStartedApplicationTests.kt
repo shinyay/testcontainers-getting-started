a45930e617c13b552dbe1792d8234ae79b844eda
shinyay <shinya.com@gmail.com>
2020-10-31 21:04:52 +0900

    Add spring property configuration

--- src/test/kotlin/com/google/shinyay/TestcontainersGettingStartedApplicationTests.kt
+++ src/test/kotlin/com/google/shinyay/TestcontainersGettingStartedApplicationTests.kt
@@ -2,6 +2,7 @@ package com.google.shinyay
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.DynamicPropertyRegistry
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
@@ -17,6 +18,12 @@ class TestcontainersGettingStartedApplicationTests {
 			it.withDatabaseName("testdb")
 			it.withUsername("scott")
 			it.withPassword("tiget")
+		}
+
+		fun properties(registry: DynamicPropertyRegistry) {
+			registry.add("spring.datasource.url", container::getJdbcUrl)
+			registry.add("spring.datasource.username", container::getUsername)
+			registry.add("spring.datasource.password", container::getPassword)
 		}
 	}
 
