512f2973966340668dc2f3acf72ed3ad0f37fff3
shinyay <shinya.com@gmail.com>
2020-10-31 22:51:25 +0900

    Add test function to validate find all books

--- src/test/kotlin/com/google/shinyay/TestcontainersGettingStartedApplicationTests.kt
+++ src/test/kotlin/com/google/shinyay/TestcontainersGettingStartedApplicationTests.kt
@@ -1,8 +1,10 @@
 package com.google.shinyay
 
+import com.google.shinyay.entity.Book
 import com.google.shinyay.repository.BookRepository
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.web.reactive.server.WebTestClient
@@ -35,7 +37,18 @@ class TestcontainersGettingStartedApplicationTests(val repository: BookRepositor
 
 	@Test
 	fun shouldReturnAllBooks() {
+		repository.save(Book(1L, "01", "Java"))
+		repository.save(Book(2L, "02", "Kotlin"))
 
+		webTestClient.get()
+				.uri("/books")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectBody()
+				.jsonPath("$.length()").isEqualTo(2)
+				.jsonPath("$[0].id").isEqualTo(1)
+				.jsonPath("$[0].isbn").isEqualTo("01")
+				.jsonPath("$[0].title").isEqualTo("Java")
 	}
 
 }
