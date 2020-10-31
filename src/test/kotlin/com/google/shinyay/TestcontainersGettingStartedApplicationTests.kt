e9f9dcc7c83975ac39f0fcf94b90834ad68b2c56
shinyay <shinya.com@gmail.com>
2020-10-31 22:41:16 +0900

    Add constructor injection

--- src/test/kotlin/com/google/shinyay/TestcontainersGettingStartedApplicationTests.kt
+++ src/test/kotlin/com/google/shinyay/TestcontainersGettingStartedApplicationTests.kt
@@ -1,16 +1,19 @@
 package com.google.shinyay
 
+import com.google.shinyay.repository.BookRepository
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
+import org.springframework.test.web.reactive.server.WebTestClient
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class TestcontainersGettingStartedApplicationTests {
+class TestcontainersGettingStartedApplicationTests(val repository: BookRepository,
+												   val webTestClient: WebTestClient) {
 
 	companion object {
 
@@ -28,6 +31,11 @@ class TestcontainersGettingStartedApplicationTests {
 			registry.add("spring.datasource.username", container::getUsername)
 			registry.add("spring.datasource.password", container::getPassword)
 		}
+	}
+
+	@Test
+	fun shouldReturnAllBooks() {
+
 	}
 
 }
