c70cf7d3af3dba795d2de2b7d7599ccf3f299490
shinyay <shinya.com@gmail.com>
2020-10-31 21:06:08 +0900

    Add Dynamic Property Source in stead of @ClassRule for Junit4

--- src/test/kotlin/com/google/shinyay/TestcontainersGettingStartedApplicationTests.kt
+++ src/test/kotlin/com/google/shinyay/TestcontainersGettingStartedApplicationTests.kt
@@ -3,6 +3,7 @@ package com.google.shinyay
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
@@ -20,6 +21,7 @@ class TestcontainersGettingStartedApplicationTests {
 			it.withPassword("tiget")
 		}
 
+		@DynamicPropertySource
 		fun properties(registry: DynamicPropertyRegistry) {
 			registry.add("spring.datasource.url", container::getJdbcUrl)
 			registry.add("spring.datasource.username", container::getUsername)
