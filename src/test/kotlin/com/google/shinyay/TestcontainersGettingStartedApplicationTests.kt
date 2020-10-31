c94d7581f7b585d8e05b7a3d1970c0bcc93ee2a0
shinyay <shinya.com@gmail.com>
2020-10-31 21:20:18 +0900

    Modify imports to optimize

--- src/test/kotlin/com/google/shinyay/TestcontainersGettingStartedApplicationTests.kt
+++ src/test/kotlin/com/google/shinyay/TestcontainersGettingStartedApplicationTests.kt
@@ -1,6 +1,5 @@
 package com.google.shinyay
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
